d2c4300ea41d43129cc1b083ee9ba99fc5c082b6
Tran Luong <tranluong@Trans-MacBook-Air.local>
2021-04-16 13:39:51 -0700

    partial working with users and conversations

--- controllers/messages.js
+++ controllers/messages.js
@@ -17,26 +17,24 @@ const createMessage = async (req,res) => {
             sender,
         }
         //look for user sender by email
-        let foundSender = await User.find({email: sender})
+        let foundSender = await User.findById(sender)
         //look for user receiver by email
-        let foundReceiver = await User.find({email: receiver})
+        let foundReceiver = await User.findById(receiver)
 
         //look in conversation for user id of person receiving it?  if id exists then push message to message//need to refactor as it needs to account for msg person has with other people...//maybe users in convo can be an array and it has to match for sender AND receiver 
         let foundConversation = await Conversation.find({ users: { $all: [foundReceiver._id, foundSender._id]}})
-
         // let foundConversation = await Conversation.find({ $or: [{userOneId: foundReceiver._id}, {userTwoId: foundReceiver._id}]})
 
-        if (!foundConversation) {
+        if (foundConversation.length === 0) {
               //if conversation is not found then create conversation
-            let newConversation = await Conversation.create({user1Id: {foundSender, userId: foundReceiver, message: []}})
-
+            let newConversation = await Conversation.create({users: [foundSender, foundReceiver], message: []})
             //add conversation id to each user
-            await User.findIdAndUpdate(
-                {_id: foundSender[0]._id},
+            await User.findByIdAndUpdate(
+                {_id: foundSender._id},
                 {$push: {conversation: newConversation._id}})
 
-            await User.findIdAndUpdate(
-                {_id: foundReceiver[0]._id},
+            await User.findByIdAndUpdate(
+                {_id: foundReceiver._id},
                 {$push: {conversation: newConversation._id}})
             //push new message into conversation messages
             let msg = await Message.create(newMessage)
@@ -52,62 +50,63 @@ const createMessage = async (req,res) => {
                 {$push: {messages: msg._id}}
             )
         }
+        return res.status(201).send("success")
     } catch (err) {
         return res.status(500).json({error: err.message})
     }
 }
 
-//TEST
-let body = {
-    content: "hello",
-    receiver: "user10",
-    sender: "user20",
-    id: "2"
-}
+// //TEST
+// let body = {
+//     content: "hello",
+//     receiver: "user10",
+//     sender: "user20",
+//     id: "2"
+// }
 
-let user = {
-    name: "user10",
-    conversation: ["123", "456"]
-}
+// let user = {
+//     name: "user10",
+//     conversation: ["123", "456"]
+// }
 
-let user2 = {
-    name: "user20",
-    conversation: ["123", "456"]
-}
-let conversation = {
-    user1: "user10",
-    user2: "user20",
-    messages: [{content: "goodbye", receiver:"user1", sender: "user2", id:"1"}],
-    id: "456"
-}
-//create message
-const testMessage = (body, conversation, user, user2) => {
-        let {content, receiver, sender, id} = body
-        let newMessage = {
-            content,
-            receiver,
-            sender,
-            id,
-        }
-        //look in conversation for matching receiver? (and sender??); that means they have a conversation
-        Object.keys(conversation).forEach((item) => {
-            if(conversation[item] === "user20") {
-                console.log("exist")
-            }
-        })
-        //if found then update conversation with message model
-        conversation.messages.push(newMessage)
-        console.log(body)
-        console.log(conversation.messages)
+// let user2 = {
+//     name: "user20",
+//     conversation: ["123", "456"]
+// }
+// let conversation = {
+//     user1: "user10",
+//     user2: "user20",
+//     messages: [{content: "goodbye", receiver:"user1", sender: "user2", id:"1"}],
+//     id: "456"
+// }
+// //create message
+// const testMessage = (body, conversation, user, user2) => {
+//         let {content, receiver, sender, id} = body
+//         let newMessage = {
+//             content,
+//             receiver,
+//             sender,
+//             id,
+//         }
+//         //look in conversation for matching receiver? (and sender??); that means they have a conversation
+//         Object.keys(conversation).forEach((item) => {
+//             if(conversation[item] === "user20") {
+//                 console.log("exist")
+//             }
+//         })
+//         //if found then update conversation with message model
+//         conversation.messages.push(newMessage)
+//         console.log(body)
+//         console.log(conversation.messages)
 
-        //if not found then create new conversation
-        //await Conversation.create() >> reference message
-        //await Message.create() 
-        //push message to conversation messages array
-        //find users and update their conversation array 
-}
+//         //if not found then create new conversation
+//         //await Conversation.create() >> reference message
+//         //await Message.create() 
+//         //push message to conversation messages array
+//         //find users and update their conversation array 
+// }
 
-testMessage(body, conversation, user, user2)
+// testMessage(body, conversation, user, user2)
 
 //get messages
 const getMessages = async (req,res) => {
--- routes/messages.js
+++ routes/messages.js
@@ -4,4 +4,5 @@ const {createMessage} = require("../controllers/messages.js")
 const router = Router()
 
 router.post("/", createMessage)
+
 module.exports = router
