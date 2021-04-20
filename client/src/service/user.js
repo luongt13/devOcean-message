1992efc1b4df752e8199a92ba9d1c036e78b3bb1
dominiquejohnsonwr <81428925+dominiquejohnsonwr@users.noreply.github.com>
2021-04-20 07:49:24 -0700

    Added updateUser to service/user

--- client/src/service/user.js
+++ client/src/service/user.js
@@ -44,3 +44,8 @@ export const getUser = async (id) => {
 }
 
 //edit user
+export const updateUser = async (id) => {
+  const response = await axios.put(`${apiURL}/users/${id}`)
+  const updatedUser = response.data
+  return updatedUser
+}
