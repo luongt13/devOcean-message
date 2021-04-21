74faa67920baad10597b770fe5fa2de27fdcf0ea
Tran Luong <tranluong@Trans-MacBook-Air.local>
2021-04-21 11:30:55 -0700

    updates

--- client/src/components/CreateMessage/CreateMessage.jsx
+++ client/src/components/CreateMessage/CreateMessage.jsx
@@ -6,6 +6,7 @@ import Search from "../Search/Search.jsx"
 import "./CreateMessage.css"
 
 export default function CreateMessage(props) {
+    console.log(props.sendTo)
     let [users, setUsers] = useState([])
     let [filteredUsers, setFilteredUsers] = useState([]);
     let [searchTerm, setSearchTerm] = useState("");
