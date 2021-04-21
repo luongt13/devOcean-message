09218230660435859e6aa2ee9153be11d9f5dc1d
Tran Luong <tranluong@Trans-MacBook-Air.local>
2021-04-21 13:20:14 -0700

    test

--- client/src/components/MessageDetails/MessageDetails.jsx
+++ client/src/components/MessageDetails/MessageDetails.jsx
@@ -10,8 +10,9 @@ export default function MessageDetails(props) {
     const [isToggled, toggle] = useToggle(false)
     const [messages, setMessages] = useState([])
     const [users, setUsers] = useState()
+    const [displayName, setDisplayName] = useState()
     let {id} = useParams()
-
+console.log(users)
     useEffect(() => {
         getData()
     }, [isToggled])
@@ -21,18 +22,30 @@ export default function MessageDetails(props) {
         setMessages(data.messages)
         setUsers(data.users)
     }
-  console.log(messages)
-  
-  async function handleDelete(item) {
-    let message = item._id
-    deleteMessage(message)
-    getData()
-  }
 
+    let receiveName = []
+    if(users) {
+        users.forEach((item) => {
+            if(item._id !== props.userData) {
+                receiveName.push(item.name)
+            }
+        })
+    }
+    console.log(receiveName)
+    if (receiveName.length === 1 ) {
+        setDisplayName(receiveName.toString())
+    }
+    // console.log(receiveName)
+    async function handleDelete(item) {
+        let message = item._id
+        deleteMessage(message)
+        getData()
+    }
 //sort messages>
 //if sender is from user logged in then make it different somehow
     return (
         <div className="message-page">
+            <h1>{displayName}</h1>
             <div className="messages">
             {messages.map(item => {
                 if (item.sender._id === props.userData) {
